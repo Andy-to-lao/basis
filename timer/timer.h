4ee36f6eb332a1662ffcbba32f79eb426bfb36c6
Project collection <52391323+Andy-to-lao@users.noreply.github.com>
2020-05-07 14:35:41 +0800

    Create timer.h

---
+++ timer/timer.h
@@ -0,0 +1,42 @@
+#ifndef SRC_UTILS_TIMER_H_
+#define SRC_UTILS_TIMER_H_
+#include <chrono>
+namespace depth_clustering {
+namespace time_utils {
+using MicroSecs = std::chrono::microseconds;
+using MilliSecs = std::chrono::milliseconds;
+using Clock = std::chrono::high_resolution_clock;
+using TimePoint = std::chrono::time_point<Clock>;
+using std::chrono::duration_cast;
+
+/**
+ * @brief      A small utility time measurement class
+ */
+class Timer {
+ public:
+  enum class Units { Micro, Milli };
+  void start() { _start = Clock::now(); }
+  uint64_t measure(const Units& units = Units::Micro) {
+    auto end = Clock::now();
+    uint64_t res = 0;
+    switch (units) {
+      case Units::Micro:
+        res = duration_cast<MicroSecs>(end - _start).count();
+        break;
+      case Units::Milli:
+        res = duration_cast<MilliSecs>(end - _start).count();
+        break;
+    }
+    _start = Clock::now();
+    return res;
+  }
+
+ private:
+  TimePoint _start = Clock::now();
+};
+
+}  // namespace time_utils
+
+}  // namespace depth_clustering
+
+#endif  // SRC_UTILS_TIMER_H_
